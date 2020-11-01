db255eab531b53a7cc57e7a716094def5c3383ac
MichaelOmondi <omondimike11@gmail.com>
2020-11-01 17:05:30 +0300

    create signup views

--- Hood/views.py
+++ Hood/views.py
@@ -38,3 +38,23 @@ def capetown(request):
 
     return render(request, 'capetown.html',{'posts':posts})
 
+# Signup
+def signup(request):
+    if request.user.is_authenticated():
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                to_email = form.cleaned_data.get('email')
+                send_activation_email(user, current_site, to_email)
+                return HttpResponse('Confirm your email address to complete registration')
+        else:
+            form = SignupForm()
+            return render(request, 'registration/signup.html',{'form':form})
+
+
