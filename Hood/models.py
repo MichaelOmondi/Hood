85587b61ee1816103fa35d0eb545d20184a28059
MichaelOmondi <omondimike11@gmail.com>
2020-11-01 16:57:05 +0300

    create a comment model

--- Hood/models.py
+++ Hood/models.py
@@ -9,8 +9,43 @@ from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
 # Profile
-class Profile(models.Model):
+ class Profile(models.Model):
     prof_pic = ImageField(blank=True, manual_crop='800x800')
     bio = HTMLField()
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
+
+    def save_profile(self):
+        self.save()
     
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+
+# comments
+class Comments(models.Model):
+    comment = HTMLField()
+    posted_on = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+    
+    @classmethod
+    def get_comments_by_posts(cls, id):
+        comments = Comments.objects.filter(post__pk = id)
+        return comments
+        
+
+
